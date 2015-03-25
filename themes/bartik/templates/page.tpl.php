c35a595c47a03e8060eb2c9029f271333f9a6053
Morten Halling <mortenh@gmx.com>
2015-03-25 21:10:40 +0100

    morten sync test
    test

--- themes/bartik/templates/page.tpl.php
+++ themes/bartik/templates/page.tpl.php
@@ -237,7 +237,7 @@
     <?php endif; ?>
 
     <?php if ($page['footer']): ?>
-      <div id="footer" class="clearfix">
+      <div id="footer" class="clearfix"> morten sync test
         <?php print render($page['footer']); ?>
       </div> <!-- /#footer -->
     <?php endif; ?>
