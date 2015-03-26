7c46966b79bda7a33ef5aaee8aba1569ad10456e
florinyaco <iacoboae.florin@gmail.com>
2015-03-26 11:47:19 +0100

    test
    testbla

--- themes/bartik/templates/page.tpl.php
+++ themes/bartik/templates/page.tpl.php
@@ -3,7 +3,7 @@
 /**
  * @file
  * Bartik's theme implementation to display a single Drupal page.
- *
+ * BLA BLA BLA
  * The doctype, html, head and body tags are not in this template. Instead they
  * can be found in the html.tpl.php template normally located in the
  * modules/system directory.
